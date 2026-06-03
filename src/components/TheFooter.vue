bdb0764ac6789f25e59e23529060c70a20807d2e
lexst64 <oleksii.stoliarchuk@icloud.com>
2026-06-03 19:22:41 +0200

    fix: update footer links to point to the correct repository

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,14 +7,16 @@ import InlineLink from './common/InlineLink.vue'
     <p>
       &copy; 2026 Oleksii Stoliarchuk. Distributed under the
       <InlineLink
-        href="https://github.com/lexst64/simple-converter-private/blob/530d1201c31b5856107b0520959653e8537648a7/LICENSE"
+        href="https://github.com/lexst64/simple-converter/blob/530d1201c31b5856107b0520959653e8537648a7/LICENSE"
       >
         MIT License </InlineLink
       >.
     </p>
     <nav class="hidden md:block">
       <ul class="flex list-none gap-3.5">
-        <li><InlineLink href="https://github.com/lexst64/simple-converter-private">Source code</InlineLink></li>
+        <li>
+          <InlineLink href="https://github.com/lexst64/simple-converter">Source code</InlineLink>
+        </li>
         <li><InlineLink href="#">FAQ</InlineLink></li>
         <li><InlineLink href="#">Contact</InlineLink></li>
       </ul>
