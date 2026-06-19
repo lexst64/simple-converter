ad240bc03bf3f9d0c2bd7281e7a2555783896d4c
lexst64 <oleksii.stoliarchuk@icloud.com>
2026-06-19 12:19:49 +0200

    feat: update version in package.json and modify footer to display version number

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import InlineLink from './common/InlineLink.vue'
+import pkg from '../../package.json';
 
 const year = computed(() => new Date().getFullYear())
 </script>
 
 <template>
   <footer class="flex justify-between bg-gray-200 p-1 text-sm">
-    <p>
-      &copy; {{ year }} Oleksii Stoliarchuk. Distributed under the
-      <InlineLink
-        href="https://github.com/lexst64/simple-converter/blob/530d1201c31b5856107b0520959653e8537648a7/LICENSE"
-      >
-        MIT License </InlineLink
-      >.
-    </p>
+    <p>v{{ pkg.version }} &copy; {{ year }} Oleksii Stoliarchuk</p>
     <nav class="hidden md:block">
       <ul class="flex list-none gap-3.5">
         <li>
